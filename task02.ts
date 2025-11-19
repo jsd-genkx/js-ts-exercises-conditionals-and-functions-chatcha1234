44040727d48fa7a2c85ad4214233f367529a1a3f
chatcha1234 <chatchawan.da1@gmail.com>
2025-11-19 03:56:38 -0500

    First commit

--- task02.ts
+++ task02.ts
@@ -1,5 +1,13 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
-
-const userInput = "42";
+const userInput = 45;
 // Try changing to "hello", 10, null, NaN
+
+
+const value = Number(userInput);
+if (!isNaN(value)) {
+    console.log("It is number");
+}
+else {
+    console.log("Not number");
+}
